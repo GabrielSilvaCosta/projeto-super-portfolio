5c8b4174da5e72fd2c6c170375da394c7faa4ec3
GabrielSilvaCosta <bob@>
2024-01-26 11:49:16 -0300

    verificando requesito 4...

--- projects/views.py
+++ projects/views.py
@@ -1,15 +1,30 @@
 # projects/views.py
 
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from django.shortcuts import render
 from .models import Profile, Project
 from .serializers import ProfileSerializer, ProjectSerializer
-from rest_framework.permissions import IsAuthenticated
 
 
 class ProfileListCreateView(viewsets.ModelViewSet):
     queryset = Profile.objects.all()
     serializer_class = ProfileSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action == "retrieve" and self.request.method == "GET":
+            return [AllowAny()]
+        return super().get_permissions()
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        if request.method == "GET":
+            return render(
+                request, "profile_detail.html", {"profile": serializer.data}
+            )
+        return super().retrieve(request, *args, **kwargs)
 
 
 class ProjectListCreateView(viewsets.ModelViewSet):
